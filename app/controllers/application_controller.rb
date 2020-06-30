f8a2bb4ae5ae3098a423b6898d3fd3f16329a792
“irobetson” <ianrobertson427@gmail.com>
2020-06-29 21:10:40 -0500

    Trying fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery with: :null_session
+  def first_contact
+  @contacts = Contact.first
+  render 'contactfiles.json.rb'
+  end
 end
